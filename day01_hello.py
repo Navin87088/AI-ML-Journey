11b1fa538777f89f1e08f14a8dacc19ca483c821
Navin87088 <navinsingh87088@gmail.com>
2026-06-19 16:26:07 +0530

    Add basic Python print statements and comments

--- day01_hello.py
+++ day01_hello.py
@@ -1,1 +1,12 @@
+#First Run in Python.
 print("Hello World")
+
+#simple python My name is Navin Singh.
+name = "Navin Singh"
+print(name)
+
+#python automatically do the math inside the bracket.
+print(3+5)
+
+#Anything in "" considered as a text and ouput will be same as in "".
+print("3+2")
